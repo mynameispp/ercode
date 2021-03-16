1efc8c9daf75459841a1717297adb045a3b75e39
33086815@qq.com <13161819>
2021-03-16 14:54:34 +0800

    1.优化生成二维码代码 2.添加加载本地二维码功能 3.添加保存生成的二维码图片到本地

--- demo/src/main/java/com/pipi/demo/MainActivity.java
+++ demo/src/main/java/com/pipi/demo/MainActivity.java
@@ -1,34 +1,107 @@
 package com.pipi.demo;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.pipi.scancode.activity.CaptureActivity;
+import com.pipi.scancode.encoding.EncodingHandler;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.ref.SoftReference;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import com.pipi.scancode.activity.CaptureActivity;
-
 public class MainActivity extends AppCompatActivity {
-    TextView result;
+    private TextView result;//扫码结果
+    private EditText inputContent;//输入内容
+    private ImageView codeImage;//生成内容的二维码
+
+    private String SAVE_PIC_PATH;//保存到SD卡
+    private Bitmap erCodeBitmap;//生成的二维码图
+    private MyHandler myHandler;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         result = findViewById(R.id.scan_code_result);
+        inputContent = findViewById(R.id.create_code_ed);
+        codeImage = findViewById(R.id.create_code_img);
+//        codeImage.setImageBitmap(BitmapFactory.decodeFile("/storage/emulated/0/DCIM/Camera/1615876167868.jpg"));
         findViewById(R.id.scan_code).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.VIBRATE,Manifest.permission.READ_EXTERNAL_STORAGE
-                        ,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.VIBRATE, Manifest.permission.READ_EXTERNAL_STORAGE
+                        , Manifest.permission.WRITE_EXTERNAL_STORAGE};
                 //申请权限，其中RC_PERMISSION是权限申请码，用来标志权限申请的
                 ActivityCompat.requestPermissions(MainActivity.this, permissions, 100);
+            }
+        });
+        findViewById(R.id.create_code_img_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //创建二维码
+                String content = inputContent.getText().toString().trim();
+                if (TextUtils.isEmpty(content)) {
+                    Toast.makeText(view.getContext(), "内容不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                erCodeBitmap = EncodingHandler.createQRCodeBitmap(content, 150, 150, "utf-8"
+                        , "H", "1", Color.BLUE, Color.WHITE);
+                codeImage.setImageBitmap(erCodeBitmap);
+
+            }
+        });
+
+        findViewById(R.id.save_create_code_img_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                //保存创建二维码
+                if (null == myHandler) {
+                    myHandler = new MyHandler(new SoftReference<Context>(view.getContext()));
+                }
+//                SAVE_PIC_PATH = view.getContext().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath()
+//                        + "/ercode/" + System.currentTimeMillis() + ".jpg";
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            SAVE_PIC_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath() + "/";
+                            saveBitmap(view.getContext(),erCodeBitmap,System.currentTimeMillis() + ".jpg");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            myHandler.sendEmptyMessage(-1);
+                        }
+                    }
+                }).start();
+
             }
         });
     }
@@ -42,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
                 && grantResults[1] == PackageManager.PERMISSION_GRANTED
                 && grantResults[2] == PackageManager.PERMISSION_GRANTED
                 && grantResults[3] == PackageManager.PERMISSION_GRANTED) {
-            startActivityForResult(new Intent(this, MyCaptureActivity.class),1);
+            startActivityForResult(new Intent(this, MyCaptureActivity.class), 1);
         } else {
             result.setText("获取权限失败");
         }
@@ -53,6 +126,101 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == CaptureActivity.RESULT_CODE_QR_SCAN && data != null && data.getExtras() != null) {
             result.setText(data.getExtras().getString(CaptureActivity.INTENT_EXTRA_KEY_QR_SCAN));
+        }
+    }
+
+    public void saveFile(Bitmap bitmap, String path, String bitName) throws Exception {
+        File file = new File(path, bitName);
+        file.mkdirs();
+        if (file.exists()) {
+            file.delete();
+        }
+        file.createNewFile();
+        FileOutputStream out;
+        try {
+            out = new FileOutputStream(file);
+            if (bitmap.compress(Bitmap.CompressFormat.PNG, 90, out)) {
+                out.flush();
+                out.close();
+                //保存图片后发送广播通知更新数据库
+                // Uri uri = Uri.fromFile(file);
+                // sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri));
+                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri uri = Uri.fromFile(file);
+                intent.setData(uri);
+                sendBroadcast(intent);
+                myHandler.sendEmptyMessage(1);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveBitmap(Context context, Bitmap  bitmap, String bitName) {
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.DESCRIPTION, "二维码");
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, bitName);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg");
+        values.put(MediaStore.Images.Media.TITLE, bitName);
+        values.put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/Camera");
+
+        Uri external = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        ContentResolver resolver = context.getContentResolver();
+
+        Uri insertUri = resolver.insert(external, values);
+        OutputStream os = null;
+        if (insertUri != null) {
+            try {
+                os = resolver.openOutputStream(insertUri);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 90, os);
+                myHandler.sendEmptyMessage(1);
+            } catch (IOException e) {
+                e.printStackTrace();
+                myHandler.sendEmptyMessage(-1);
+            }finally {
+                try {
+                    if (os != null) {
+                        os.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class MyHandler extends Handler {
+        SoftReference<Context> contextSoftReference;
+
+        public MyHandler(SoftReference<Context> contextSoftReference) {
+            super();
+            this.contextSoftReference = contextSoftReference;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 1) {
+                Toast.makeText(contextSoftReference.get(), "保存成功", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(contextSoftReference.get(), "无法保存当前图", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != myHandler) {
+            myHandler.removeCallbacksAndMessages(null);
+            myHandler = null;
+        }
+        if (erCodeBitmap != null) {
+            erCodeBitmap.recycle();
+            erCodeBitmap = null;
         }
     }
 }
--- demo/src/main/java/com/pipi/demo/MyCaptureActivity.java
+++ demo/src/main/java/com/pipi/demo/MyCaptureActivity.java
@@ -1,12 +1,24 @@
 package com.pipi.demo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.pipi.demo.tools.EaseFileUtils;
+import com.pipi.demo.tools.VersionUtils;
 import com.pipi.scancode.activity.CaptureActivity;
 import com.pipi.scancode.view.ViewfinderView;
+
+import java.io.File;
+
+import androidx.annotation.Nullable;
 
 public class MyCaptureActivity extends CaptureActivity implements SurfaceHolder.Callback {
     private ImageView back;
@@ -30,10 +42,43 @@ public class MyCaptureActivity extends CaptureActivity implements SurfaceHolder.
         scanner_toolbar_album.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //本地图片，建议扫码是让用户裁剪二维码
-//                scanAlbunUrl("/storage/emulated/0/Pictures/Screenshots/Screenshot_20200330_171027.jpg");
+                //本地图片，打开本地相册，建议扫码是让用户裁剪二维码
+                Intent intent = null;
+                if(VersionUtils.isTargetQ(view.getContext())) {
+                    intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                }else {
+                    intent = new Intent(Intent.ACTION_GET_CONTENT);
+                }
+                intent.setType("image/*");
+                startActivityForResult(intent, 100);
             }
         });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode == 100) {
+            onActivityResultForLocalPhotos(data);
+        }
+    }
+    /**
+     * 选择本地图片处理结果
+     *
+     * @param data
+     */
+    protected void onActivityResultForLocalPhotos(@Nullable Intent data) {
+        if (data != null) {
+            Uri selectedImage = data.getData();
+            if (selectedImage != null) {
+                String filePath = EaseFileUtils.getFilePath(this, selectedImage);
+                Log.e("图片路径",filePath);
+                if (!TextUtils.isEmpty(filePath) && new File(filePath).exists()) {
+                    scanAlbunUrl(filePath);
+                }else {
+                    Toast.makeText(this, "扫描失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
 
